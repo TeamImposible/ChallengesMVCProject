3833b525fdbc0c958cd18f3d0ea53eb7618aa337
nikigmy <nikigmy@gmail.com>
2016-07-09 00:37:00 +0300

    Added model, view, controller and migration for the challenges

---
+++ Challenger/Migrations/201607082006097_Challenge.cs
@@ -0,0 +1,18 @@
+namespace Challenger.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Challenge : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Tasks", "DueDate", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Tasks", "DueDate", c => c.String());
+        }
+    }
+}
---
+++ Challenger/Models/ChallengeViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Challenger.DataLayer;
+
+namespace Challenger.Models
+{
+    public class CreateChallengeViewModel
+    {
+        public string Title { get; set; }
+        public Task.ChallengeType Type { get; set; }
+    }
+}
